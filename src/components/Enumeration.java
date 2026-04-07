93dde82f3036d6d41a85b6188d70afad5c44ff38
piedadlambea <piedadlambea@gmail.com>
2026-04-07 21:13:30 +0200

    0.6 Dependency

---
+++ src/components/Enumeration.java
@@ -0,0 +1,61 @@
+/**
+ */
+package components;
+
+import org.eclipse.emf.common.util.EList;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Enumeration</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * </p>
+ * <ul>
+ *   <li>{@link components.Enumeration#getStereotype <em>Stereotype</em>}</li>
+ *   <li>{@link components.Enumeration#getLiterals <em>Literals</em>}</li>
+ * </ul>
+ *
+ * @see components.ComponentsPackage#getEnumeration()
+ * @model annotation="gmf.node label='name' figure='rectangle' color='200,255,200'"
+ * @generated
+ */
+public interface Enumeration extends Classifier {
+	/**
+	 * Returns the value of the '<em><b>Stereotype</b></em>' attribute.
+	 * The default value is <code>"<<enumeration>>"</code>.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Stereotype</em>' attribute.
+	 * @see #setStereotype(String)
+	 * @see components.ComponentsPackage#getEnumeration_Stereotype()
+	 * @model default="&lt;&lt;enumeration&gt;&gt;"
+	 * @generated
+	 */
+	String getStereotype();
+
+	/**
+	 * Sets the value of the '{@link components.Enumeration#getStereotype <em>Stereotype</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Stereotype</em>' attribute.
+	 * @see #getStereotype()
+	 * @generated
+	 */
+	void setStereotype(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Literals</b></em>' containment reference list.
+	 * The list contents are of type {@link components.EnumLiteral}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Literals</em>' containment reference list.
+	 * @see components.ComponentsPackage#getEnumeration_Literals()
+	 * @model containment="true"
+	 *        annotation="gmf.compartment layout='list'"
+	 * @generated
+	 */
+	EList<EnumLiteral> getLiterals();
+
+} // Enumeration
