5bbbe77bac2ba63096def6d0a4b10588b5476230
piedadlambea <piedadlambea@gmail.com>
2026-07-02 13:06:08 +0200

    validaciones corrigiendo

--- gettw.diagram/src/gettw/diagram/edit/parts/CustomShapeEditPart.java
+++ gettw.diagram/src/gettw/diagram/edit/parts/CustomShapeEditPart.java
@@ -132,7 +132,8 @@ public class CustomShapeEditPart extends AbstractBorderedShapeEditPart {
 	protected void addBorderItem(IFigure borderItemContainer, IBorderItemEditPart borderItemEditPart) {
 		if (borderItemEditPart instanceof CustomShapeId_EditPart) {
 			BorderItemLocator locator = new BorderItemLocator(getMainFigure(), PositionConstants.SOUTH);
-			locator.setBorderItemOffset(new Dimension(-8, -8));			borderItemContainer.add(borderItemEditPart.getFigure(), locator);
+			locator.setBorderItemOffset(new Dimension(-20, -20));
+			borderItemContainer.add(borderItemEditPart.getFigure(), locator);
 		} else {
 			super.addBorderItem(borderItemContainer, borderItemEditPart);
 		}
